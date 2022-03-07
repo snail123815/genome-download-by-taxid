7295d337a8bb00254d9fc0002d6eda70759eb545
snail123815 <durand.dc@hotmail.com>
2022-03-07 14:40:40 +0100

    elink by step, keep tracking info in file

--- fetch_nucl_by_taxid.py
+++ fetch_nucl_by_taxid.py
@@ -13,26 +13,117 @@ from Bio import Seq
 import subprocess
 import logging
 
+
 from sklearn.utils import all_estimators
 from calHash_on_args import calHash
 import pickle
 
 
 class RecordIO:
-    def __init__(self, file):
+    def __init__(self, file, prefix='IDs from source id groups '):
         self.file = file
+        self.pf = prefix.strip() + ' '
+        self._completeMark = "ALL"
+        self.logger = logging.getLogger()
+        try:
+            with open(self.file, 'r') as fh:
+                headerLine = fh.readline()
+        except FileNotFoundError:
+            headerLine = ""
+        self.gn, self.nGroups, self.completed = self._readHeader(headerLine)
+        if self.nGroups == -1 and not self.completed:
+            with open(self.file, 'w') as fh:
+                fh.write(self.pf + f'{self.gn}/{self.nGroups}' + '\n')
+                # done/total
+    
+    def _readHeader(self, headerLine):
+        pf_from_file = ' '.join(headerLine.split(' ')[:-1])
+        gn = 0
+        nGroups = -1
+        completed = False
+        if pf_from_file == self.pf[:-1]:
+            mark = headerLine.split(' ')[-1].strip()
+            if mark == self._completeMark:
+                gn = -1
+                nGroups = -1
+                completed = True
+            else:
+                gn, nGroups = mark.split('/')
+                gn = int(gn)
+                nGroups = int(nGroups)
+                assert gn < nGroups or nGroups == -1, f'File content error, {gn} should < {nGroups}'
+                completed = False
+        elif pf_from_file == '':
+            pass
+        else:
+            self.logger.warning(f'File prefix changed, file will be overwritten.')
+        return gn, nGroups, completed
     
     def writeList(self, l):
-        logger.info(f'   Write list to file {self.file}.')
+        self.logger.info(f'   Write whole list to file {self.file}.')
         with open(self.file, 'w') as fh:
+            fh.write(self.pf + self._completeMark + "\n")
             fh.writelines(f'{i}\n' for i in l)
-    
+        self.completed = True
+
     def readList(self):
         l = []
         with open(self.file, 'r') as fh:
-            logger.info('   Reading from previous.')
+            headerLine = fh.readline()
+            self.gn, nGroups, self.completed = self._readHeader(headerLine)
+            if self.nGroups == -1:
+                self.nGroups = nGroups
+            else:
+                assert self.nGroups == nGroups
+            self.logger.info(f'   Reading from previous file {headerLine.strip()}.')
             [l.append(i.strip()) for i in fh]
         return l
+
+    def appendList(self, l, groupIdx=None):
+        groupIdx = groupIdx or self.gn
+        assert groupIdx == self.gn, f'Index not correct: should be {self.gn}, but {groupIdx}'
+        with open(self.file, 'r') as fh:
+            headerLine = fh.readline()
+        mark = headerLine.split(' ')[-1].strip()
+        if mark == self._completeMark:
+            self.logger.error(f'   The file is already full! Want to start again?')
+            sys.exit()
+        else:
+            gn, nGroups = mark.split('/')
+            gn = int(gn)
+            nGroups = int(nGroups)
+            if nGroups == -1 and self.nGroups == -1 and gn == 0: # only warn once
+                self.logger.warning('   Total group number not known. Please do RecordIO.nGroups = n before appending list.')
+            elif self.nGroups == -1:
+                self.nGroups = nGroups
+            elif nGroups == -1:
+                nGroups = self.nGroups
+            assert nGroups == self.nGroups, 'Target file and target list are not from the same source.'
+            assert gn == self.gn, 'Missing group or previous "sed" not successful'
+            self.gn += 1
+            if self.gn == self.nGroups:
+                self.completed = True
+            newHeader = self.pf + (f'{gn+1}/{nGroups}' if not self.completed else self._completeMark)
+            # change the first line of target file
+            cmd = ['sed', '-i', '', '-e', '1,1s/.*/' + newHeader.replace('/', '\/') + '/g', self.file]
+            subprocess.call(cmd)
+        with open(self.file, 'a') as fh:
+            fh.writelines(f'{i}\n' for i in l)
+
+
+def splitGroup(listObj, step):
+    ll = [] # list of list with maximum {step} of items each
+    if len(listObj) > step:
+        nGroups = len(listObj)//step + (1 if len(listObj)%step>0 else 0)
+        logger.info(f'   {nGroups} groups.')
+        for i in range(nGroups):
+            try:
+                ll.append(listObj[i*step:i*step+step])
+            except IndexError:
+                ll.append(listObj[i*step:])
+    else:
+        ll.append(listObj)
+    return ll
 
 
 def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
@@ -55,9 +146,8 @@ def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
     # 1st step
     # Get all child taxonomy from database
     logger.info(f'1. Fetch childs of taxid {targetTx}.')
-    try:
-        allTax = txids_io.readList()
-    except FileNotFoundError:
+    allTax = txids_io.readList()
+    if not txids_io.completed:
         handle = Entrez.elink(
             db='taxonomy',
             dbfrom='taxonomy',
@@ -76,63 +166,29 @@ def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
     # 2nd step
     # Get assemblies from taxid
     logger.info(f'2. Fetch linked assembly ids of fetched taxids')
-    eachGroup_ass = 200 # submit this number of ids in each request. Prevent dead response.
-    try:
-        allAssemblies = asmids_io.readList()
-    except FileNotFoundError:
-        if len(allTax) > eachGroup_ass:
-            taxGroups = []
-            nGroups = len(allTax)//eachGroup_ass + (1 if len(allTax)%eachGroup_ass>0 else 0)
-            logger.info(f'   {nGroups} groups.')
-            for i in range(nGroups):
-                try:
-                    taxGroups.append(allTax[i*eachGroup_ass:i*eachGroup_ass+eachGroup_ass])
-                except IndexError:
-                    taxGroups.append(allTax[i*eachGroup_ass:])
-        allAssemblies = []
-        for i, taxs in enumerate(taxGroups):
-            logger.info(f'   Getting assembly ids for group {i+1}/{nGroups}')
-            ########################## do not work for elink
-            # count = 0
-            # start = 0
-            # webenv = None
-            # retmax = 2000
-            # while start < count or webenv is None:
-            #     handle = Entrez.elink(
-            #         db='assembly',
-            #         dbfrom='taxonomy',
-            #         id=taxs,
-            #         usehistory=True,
-            #         webenv=webenv,
-            #         linkname='taxonomy_assembly',
-            #         retmax=retmax
-            #     )
-            #     record_a = Entrez.read(handle)
-            #     handle.close()
-            #     count = int(record_a['Count'])
-            #     if count == '0':
-            #         logger.warning(f'   Zero record in this group')
-            #         break
-            #     if 'WarningList' in record_a:
-            #         logger.warning(record_a['WarningList']['OutputMessage'])
-            #     if start == 0:
-            #         webenv = record_a['WebEnv']
-            #         logger.info(f'   Web accession {webenv}')
-            #         logger.info(f'   Total ids {count}.')
-            #     start += retmax
-            #     [allAssemblies.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_a if len(r['LinkSetDb']) > 0]
-            ##########################
-            handle = Entrez.elink(
-                db='assembly',
-                dbfrom='taxonomy',
-                id=taxs,
-                linkname='taxonomy_assembly',
-            )
-            record_a = Entrez.read(handle)
-            handle.close()
-            [allAssemblies.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_a if len(r['LinkSetDb']) > 0]
-            # not all taxid have corresponding assembly, many have more than 1 assembly.
-        asmids_io.writeList(allAssemblies)
+    allTax = allTax[:1400]
+    eachGroup_ass = 500 # submit this number of ids in each request. Prevent dead response.
+    taxGroups = splitGroup(allTax, eachGroup_ass)
+    allAssemblies = asmids_io.readList()
+    if asmids_io.nGroups == -1:
+        asmids_io.nGroups = len(taxGroups)
+    i = asmids_io.gn
+    while not asmids_io.completed:
+        assemblies = []
+        logger.info(f'   Getting assembly ids for group {i+1}/{asmids_io.nGroups}')
+        handle = Entrez.elink(
+            db='assembly',
+            dbfrom='taxonomy',
+            id=taxGroups[i],
+            linkname='taxonomy_assembly',
+        )
+        record_a = Entrez.read(handle)
+        handle.close()
+        [assemblies.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_a if len(r['LinkSetDb']) > 0]
+        asmids_io.appendList(assemblies, i)
+        allAssemblies.extend(assemblies)
+        i += 1
+        # not all taxid have corresponding assembly, many have more than 1 assembly.
     logger.info(f'   Total number of assemblies is {len(allAssemblies)}.')
     logger.info(f'   The first 5 are: {allAssemblies[:min(len(allAssemblies), 5)]}')
 
@@ -140,32 +196,27 @@ def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
     # Get nucleic id from refseq
     logger.info(f'3. Fetch nucl ids of fetched assemblies, only refseq.')
     eachGroup_nucl = 200 # submit this number of ids in each request. Prevent dead response.
-    try:
-        allNucl = nuclids_io.readList()
-    except FileNotFoundError:
-        if len(allAssemblies) > eachGroup_nucl:
-            assGroups = []
-            nGroups = len(allAssemblies)//eachGroup_nucl + (1 if len(allAssemblies)%eachGroup_nucl>0 else 0)
-            logger.info(f'   {nGroups} groups.')
-            for i in range(nGroups):
-                try:
-                    assGroups.append(allAssemblies[i*eachGroup_nucl:i*eachGroup_nucl+eachGroup_nucl])
-                except IndexError:
-                    assGroups.append(allAssemblies[i*eachGroup_nucl:])
-        allNucl = []
-        for i, asses in enumerate(assGroups):
-            logger.info(f'   Getting nucleotide ids for group {i+1}/{nGroups}')
-            handle = Entrez.elink(
-                db = 'nuccore',
-                dbfrom = 'assembly',
-                id=asses,
-                linkname='assembly_nuccore_refseq', # will remove most irrelevent sequences
-                term=f'{minLength}:99999999[SLEN]'
-            )
-            record_n = Entrez.read(handle)
-            handle.close()
-            [allNucl.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_n if len(r['LinkSetDb']) > 0]
-        nuclids_io.writeList(allNucl)
+    assGroups = splitGroup(allAssemblies, eachGroup_nucl)
+    allNucl = nuclids_io.readList()
+    if nuclids_io.nGroups == -1:
+        nuclids_io.nGroups = len(assGroups)
+    i = nuclids_io.gn
+    while not nuclids_io.completed:
+        nucls = []
+        logger.info(f'   Getting nucleotide ids for group {i+1}/{nuclids_io.nGroups}')
+        handle = Entrez.elink(
+            db = 'nuccore',
+            dbfrom = 'assembly',
+            id=assGroups[i],
+            linkname='assembly_nuccore_refseq', # will remove most irrelevent sequences
+            term=f'{minLength}:99999999[SLEN]'
+        )
+        record_n = Entrez.read(handle)
+        handle.close()
+        [nucls.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_n if len(r['LinkSetDb']) > 0]
+        nuclids_io.appendList(nucls)
+        allNucl.extend(nucls)
+        i += 1
     logger.info(f'   Total number of nucleotides is {len(allNucl)}.')
     logger.info(f'   The first 5 are: {allNucl[:min(len(allNucl), 5)]}')
 
@@ -182,7 +233,38 @@ if __name__ == '__main__':
     os.makedirs(targetDir, exist_ok=True)
     logging.basicConfig(filename=os.path.join(targetDir, 'Fetch_taxonomy_nucl.log'), level=logging.INFO)
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger()
 
     fetch_nucl_by_taxID(targetTx='201174', minLength=10000, targetDir=targetDir, api=args.api, email=args.email)
     # Actinobacteria, some super tax
+
+        ########################## do not work for elink
+        # count = 0
+        # start = 0
+        # webenv = None
+        # retmax = 2000
+        # while start < count or webenv is None:
+        #     handle = Entrez.elink(
+        #         db='assembly',
+        #         dbfrom='taxonomy',
+        #         id=taxs,
+        #         usehistory=True,
+        #         webenv=webenv,
+        #         linkname='taxonomy_assembly',
+        #         retmax=retmax
+        #     )
+        #     record_a = Entrez.read(handle)
+        #     handle.close()
+        #     count = int(record_a['Count'])
+        #     if count == '0':
+        #         logger.warning(f'   Zero record in this group')
+        #         break
+        #     if 'WarningList' in record_a:
+        #         logger.warning(record_a['WarningList']['OutputMessage'])
+        #     if start == 0:
+        #         webenv = record_a['WebEnv']
+        #         logger.info(f'   Web accession {webenv}')
+        #         logger.info(f'   Total ids {count}.')
+        #     start += retmax
+        #     [allAssemblies.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_a if len(r['LinkSetDb']) > 0]
+        ##########################
