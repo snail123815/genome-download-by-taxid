4a963ea29a58b0e43775be2d2ef135e01167afe4
snail123815 <durand.dc@hotmail.com>
2022-03-08 09:28:29 +0100

    Complete test setup and main functions

--- fetch_nucl_by_taxid.py
+++ fetch_nucl_by_taxid.py
@@ -1,22 +1,12 @@
-from email.errors import InvalidMultipartContentTransferEncodingDefect
 from Bio import Entrez
 import os
 import pickle
-import lzma
-from datetime import date
 from time import strftime, sleep
-from urllib.error import HTTPError
-from http.client import IncompleteRead
 import sys
-from Bio import SeqIO
-from Bio import Seq
 import subprocess
 import logging
-
-
-from sklearn.utils import all_estimators
-from calHash_on_args import calHash
 import pickle
+from calHash_on_args import calHash
 
 
 class RecordIO:
@@ -34,7 +24,6 @@ class RecordIO:
         if self.nGroups == -1 and not self.completed:
             with open(self.file, 'w') as fh:
                 fh.write(self.pf + f'{self.gn}/{self.nGroups}' + '\n')
-                # done/total
     
     def _readHeader(self, headerLine):
         pf_from_file = ' '.join(headerLine.split(' ')[:-1])
@@ -75,7 +64,7 @@ class RecordIO:
                 self.nGroups = nGroups
             else:
                 assert self.nGroups == nGroups
-            self.logger.info(f'   Reading from previous file {headerLine.strip()}.')
+            self.logger.info(f'   Reading from previous file, header: {headerLine.strip()}.')
             [l.append(i.strip()) for i in fh]
         return l
 
@@ -153,11 +142,11 @@ def elink_by_step(RecIO, allIds, db, dbfrom, linkname, term=None, step=500):
     return allTarIds
 
 
-def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
+def fetch_nucl_by_taxID(targetTx, minLen, targetDir, api, email, isTest=False):
     Entrez.api_key = api
     Entrez.email = email
-    txHash = calHash(targetTx)
-    nuclIdHash = calHash(targetTx, minLength)
+    txHash = calHash(targetTx, isTest)
+    nuclIdHash = calHash(targetTx, minLen, isTest)
 
     txids_file = os.path.join(targetDir, f'txid_{txHash}.txt')
     txids_io = RecordIO(txids_file)
@@ -166,10 +155,9 @@ def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
     nuclids_file = os.path.join(targetDir, f'nucl_{nuclIdHash}.txt')
     nuclids_io = RecordIO(nuclids_file)
 
-
     # TODO what if you want one single tax? 0 return for the first elink? or else?
 
-    logger.info(f'{" Start Fetch Nucleotides by Taxonomy ID ":=^80}')
+    logger.info(f'{" Start Getting Nucleotide IDs by Taxonomy ID ":=^80}')
     # 1st step
     # Get all child taxonomy from database
     logger.info(f'1. Fetch childs of taxid {targetTx}.')
@@ -190,16 +178,21 @@ def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
     logger.info(f'   Total number of child taxids is {len(allTax)}.')
     logger.info(f'   The first 5 are: {allTax[:5]}')
 
+    if isTest:
+        allTax = allTax[:min(len(allTax),1600)]
+
     # 2nd step
     # Get assemblies from taxid
     logger.info(f'2. Fetch linked assembly ids of fetched taxids')
-    allTax = allTax[:1400]
     allAssemblies = elink_by_step(asmids_io, allTax,
                                   db='assembly',
                                   dbfrom='taxonomy',
                                   linkname='taxonomy_assembly')
     logger.info(f'   Total number of assemblies is {len(allAssemblies)}.')
     logger.info(f'   The first 5 are: {allAssemblies[:min(len(allAssemblies), 5)]}')
+
+    if isTest:
+        allAssemblies = allAssemblies[:min(len(allAssemblies), 1100)]
 
     # 3nd step
     # Get nucleic id from refseq
@@ -208,55 +201,111 @@ def fetch_nucl_by_taxID(targetTx, minLength, targetDir, api, email):
                             db='nuccore',
                             dbfrom='assembly',
                             linkname='assembly_nuccore_refseq',
-                            term=f'{minLength}:99999999[SLEN]')
+                            term=f'{minLen}:99999999[SLEN]')
     logger.info(f'   Total number of nucleotides is {len(allNucl)}.')
     logger.info(f'   The first 5 are: {allNucl[:min(len(allNucl), 5)]}')
+    return allNucl, nuclids_io.file
+
+def fetch_nucl_by_step(targetDir, idsFile, step=200, isTest=False):
+    logger=logging.getLogger()
+    logger.info(f'{" Start fetching nucleotide data ":=^80}')
+    fetchHash = calHash(idsFile, step, isTest)
+    infoFile = os.path.join(targetDir, f'fetch_{fetchHash}.pkl')
+    logger = logging.getLogger('Fetching_nucleotide')
+    ids = RecordIO(idsFile).readList()
+    logger.info(f'Total number of ids {len(ids)}, first 5: {ids[:min(len(ids),5)]}')
+    if isTest:
+        ids = ids[:min(len(ids), 500)]
+    idGroups = splitGroup(ids, step)
+    missingList = []
+    lastGroupIdx = -1 # Means not finished at all
+    def updateInfo(lastGroupIdx, missingList):
+        with open(infoFile, 'wb') as fh:
+            pickle.dump([lastGroupIdx, missingList], fh)
+    if os.path.isfile(infoFile):
+        with open(infoFile, 'rb') as fh:
+            lastGroupIdx, missingList = pickle.load(fh)
+    else:
+        updateInfo(lastGroupIdx, missingList)
+    maxDig = len(str(len(idGroups)+1))
+    ns = list(range(lastGroupIdx+1, len(idGroups))) + missingList
+    for n in ns:
+        file = os.path.join(targetDir, f'nucl_{str(n+1).zfill(maxDig)}.gbk')
+        idFile = os.path.join(targetDir, f'nucl_{str(n+1).zfill(maxDig)}.txt')
+        trials = 3
+        success = False
+        logger.info(f'Fetching group No. {n+1}, {file}')
+        while not success and trials > 0:
+            try:
+                handle = Entrez.efetch(
+                    db='nuccore',
+                    id=idGroups[n],
+                    rettype='gbwithparts',
+                    retmode='text'
+                )
+                with open(file, 'w') as fh:
+                    fh.write(handle.read())
+                handle.close()
+                success = True
+            except:
+                sleep(2)
+                trials -= 1
+        if success:
+            with open(idFile, 'w') as fh:
+                [fh.write(str(i) + '\n') for i in idGroups[n]]
+            logger.info(f"handle.read(Finished group {n+1}: {os.stat(file).st_size/1024/1024:.2f} MB {file}")
+        else:
+            logger.info(f"3 trials not succeed for group {n+1}")
+            missingList.append(n)
+        updateInfo(n, missingList) # Number finished, update after fetching
+    return missingList
+
+
+
 
 if __name__ == '__main__':
     import argparse
 
+    scriptDir = os.path.split(os.path.realpath(__file__))[0]
+    testOutputDir = os.path.join(scriptDir, 'test_output')
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('api', type=str, help='api key from NCBI')
+    parser.add_argument('api', type=str, help='API key from NCBI')
     parser.add_argument('email', type=str, help='Email address for identify yourself')
-
+    parser.add_argument('-t', action='store_true', help='Set if you want to run a test. Only few records will be collected')
+    targets = parser.add_argument_group('Target arguments')
+    targets.add_argument('--taxIds', type=str, nargs='+', default=['201174'], help='Target taxonomyIds')
+    targets.add_argument('--minLen', type=int, default=10000, help='Discard nucleotides less than this length')
+    targets.add_argument('--outputDir', type=str, default=testOutputDir, help='Output dir')
+    
     args = parser.parse_args()
-
-    targetDir = './test_output'
-    os.makedirs(targetDir, exist_ok=True)
-    logging.basicConfig(filename=os.path.join(targetDir, 'Fetch_taxonomy_nucl.log'), level=logging.INFO)
-
+    logging.basicConfig(filename=os.path.join(args.outputDir, 'Fetch_taxonomy_nucl.log'), level=logging.INFO)
     logger = logging.getLogger()
 
-    fetch_nucl_by_taxID(targetTx='201174', minLength=10000, targetDir=targetDir, api=args.api, email=args.email)
-    # Actinobacteria, some super tax
+    # Check if test is correctly set or unset
+    if args.t:
+        logger.info(f'Doing test fetching {args.taxIds} with minimum length {args.minLen} to output dir {testOutputDir}.')
+        logger.info('Only fetch first 500 ids')
+        if args.taxIds != ['201174'] or args.minLen != 10000 or os.path.realpath(args.outputDir) != testOutputDir:
+            parser.error('If test (-t), please leave "taxIds", "minLen", and "outputDir" default.')
+    elif args.taxIds == ['201174'] or args.minLen == 10000 or os.path.realpath(args.outputDir) == testOutputDir:
+        parser.error('Arguments --taxIds, --minLen, --outputDir are required!')
+    elif os.path.realpath(args.outputDir) == testOutputDir:
+        parser.error(f'Please do not set output dir as {testOutputDir}')
 
-        ########################## do not work for elink
-        # count = 0
-        # start = 0
-        # webenv = None
-        # retmax = 2000
-        # while start < count or webenv is None:
-        #     handle = Entrez.elink(
-        #         db='assembly',
-        #         dbfrom='taxonomy',
-        #         id=taxs,
-        #         usehistory=True,
-        #         webenv=webenv,
-        #         linkname='taxonomy_assembly',
-        #         retmax=retmax
-        #     )
-        #     record_a = Entrez.read(handle)
-        #     handle.close()
-        #     count = int(record_a['Count'])
-        #     if count == '0':
-        #         logger.warning(f'   Zero record in this group')
-        #         break
-        #     if 'WarningList' in record_a:
-        #         logger.warning(record_a['WarningList']['OutputMessage'])
-        #     if start == 0:
-        #         webenv = record_a['WebEnv']
-        #         logger.info(f'   Web accession {webenv}')
-        #         logger.info(f'   Total ids {count}.')
-        #     start += retmax
-        #     [allAssemblies.extend([l['Id'] for l in r['LinkSetDb'][0]['Link']]) for r in record_a if len(r['LinkSetDb']) > 0]
-        ##########################
+    os.makedirs(args.outputDir, exist_ok=True)
+    allNucl, nuclIdsFile = fetch_nucl_by_taxID(targetTx=args.taxIds,
+                                               minLen=args.minLen,
+                                               targetDir=args.outputDir,
+                                               api=args.api, email=args.email,
+                                               isTest=args.t)
+    missingList = fetch_nucl_by_step(args.outputDir, nuclIdsFile, isTest=args.t)
+    if not args.t:
+        trials = 3
+        while len(missingList) > 0 and trials > 0:
+            trials -= 1
+            retryStr = f'Retrying {len(missingList)} missed groups. Trial No.{3-trials}'
+            logger.info(f'{retryStr:=^80}')
+            missingList = fetch_nucl_by_step(args.outputDir, nuclIdsFile)
+
+    logger.info(f'{" DONE ":=^80}')
